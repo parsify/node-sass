e7bc37dc7eb82762ca1110a0142330ea1f02ccd4
Nijiko Yonskai <nijikokun@gmail.com>
2012-12-24 13:25:22 -0800

    Update rebuild.js

--- rebuild.js
+++ rebuild.js
@@ -1,5 +1,5 @@
 var spawn = require('child_process').spawn;
 
-if (process.platform === 'darwin') {
+if (process.platform === 'darwin' || (process.platform === 'sunos' || process.platform === 'solaris')) {
   spawn('node-gyp', ['rebuild']);
 }
